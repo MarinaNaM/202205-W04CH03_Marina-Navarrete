4061fd5e53a027a70de341cd345eecd355b60876
Marina Navarrete <mnavarretem@gmail.com>
2022-06-01 21:34:52 +0200

    Update pages

--- src/pages/got-page.tsx
+++ src/pages/got-page.tsx
@@ -1,5 +1,16 @@
+import React from 'react';
+import { List } from '../components/List';
+import { PersonajesData } from '../data/personajes-data';
+
 export function GotPage() {
+    const personajes = PersonajesData();
+
     return (
-        
-    )
+        <>
+            <h1>Gome Of Thrones</h1>
+            <div className="app container">
+                <List personajes={personajes}></List>
+            </div>
+        </>
+    );
 }
